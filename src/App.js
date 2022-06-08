ffd39ee2552d0582fa22a76a380383453f923a5d
Rebecca Neimeyer <rebeccaneimeyer@Rebeccas-MacBook.local>
2022-06-08 15:22:37 -0700

    updated to add routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Characters from "./components/Characters";
+import Quotes from "./components/Quotes";
+import Episodes from "./components/Episodes";
+import Crew from "./components/Crew";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 function App() {
   const [characters, setCharacters] = useState([]);
@@ -25,8 +32,17 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Welcome to Office Trivia!</h1>
-      <Characters characters={characters} />
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/characters" element={<Characters characters={characters} />} />
+          <Route path="/quotes" element={<Quotes quotes={quotes} />} />
+          <Route path="/episodes" element={<Episodes episodes={episodes} />} />
+          <Route path="/crew" element={<Crew crew={crew} />} />
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 }
