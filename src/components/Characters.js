227d5b1725a1cb048bb2548fbd2fa6705cd30376
Rebecca Neimeyer <rebeccaneimeyer@Rebeccas-MacBook.local>
2022-06-08 15:37:31 -0700

    added search to character list

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,29 +1,52 @@
+import { useState } from "react";
+
 function Characters({ characters }) {
+  const [search, setSearch] = useState("");
 
-    const showCharacters = characters.map((character) => {
-        if (character.lastname === "null") {
-          return (
-            <div key={character._id}>
-              <h3>{character.firstname}</h3>
-            </div>
-          );
-        } else {
-          return (
-            <div key={character._id}>
-              <h3>
-                {character.firstname} {character.lastname}
-              </h3>
-            </div>
-          );
-        }
-      });
-    
-      return (
-        <div className="characters">
-          {showCharacters}
-        </div>
-      );
+  const showCharacters = characters
+    .filter((character) => {
+      const fullName = character.firstname + " " + character.lastname;
+      if (
+        character.firstname.toLowerCase().includes(search.toLowerCase()) ||
+        character.lastname.toLowerCase().includes(search.toLowerCase()) ||
+        fullName.toLowerCase().includes(search.toLowerCase())
+      ) {
+        return character;
+      }
+    })
+    .map((character) => {
+      if (character.lastname === "null") {
+        return (
+          <div key={character._id}>
+            <h3>{character.firstname}</h3>
+          </div>
+        );
+      } else {
+        return (
+          <div key={character._id}>
+            <h3>
+              {character.firstname} {character.lastname}
+            </h3>
+          </div>
+        );
+      }
+    });
 
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
+  return (
+    <div className="characters">
+      <form>
+        <input
+          placeholder="Search for your favorite character..."
+          onChange={handleSearch}
+        ></input>
+      </form>
+      {showCharacters}
+    </div>
+  );
 }
 
 export default Characters;
