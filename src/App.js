0d91d460369e27e37180c95b1701f20d2d9aa131
Rebecca Neimeyer <rebeccaneimeyer@Rebeccas-MacBook.local>
2022-06-08 11:23:00 -0700

    fetched list of characters

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetch("https://www.officeapi.dev/api/characters/")
+      .then((response) => response.json())
+      .then((data) => setCharacters(data.data));
+  }, []);
+
+  const showCharacters = characters.map((character) => {
+    if (character.lastname === "null") {
+      return (
+        <div>
+          <h3>{character.firstname}</h3>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h3>
+            {character.firstname} {character.lastname}
+          </h3>
+        </div>
+      );
+    }
+  });
+
   return (
     <div className="App">
+      <h1>Welcome to Office Trivia!</h1>
+      {showCharacters}
     </div>
   );
 }
