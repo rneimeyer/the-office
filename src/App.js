776c2ba6e10b5e89eaec7510cf926de0b1691ebd
Rebecca Neimeyer <rebeccaneimeyer@Rebeccas-MacBook.local>
2022-06-08 14:32:25 -0700

    fetching data for quotes, episodes, and crew

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import Characters from "./components/Characters";
 
 function App() {
   const [characters, setCharacters] = useState([]);
+  const [quotes, setQuotes] = useState([]);
+  const [episodes, setEpisodes] = useState([]);
+  const [crew, setCrew] = useState([])
 
   useEffect(() => {
     fetch("https://www.officeapi.dev/api/characters/")
       .then((response) => response.json())
       .then((data) => setCharacters(data.data));
+    fetch("https://www.officeapi.dev/api/quotes/")
+      .then((response) => response.json())
+      .then((data) => setQuotes(data.data));
+    fetch("https://www.officeapi.dev/api/episodes/")
+      .then((response) => response.json())
+      .then((data) => setEpisodes(data.data));
+    fetch("https://www.officeapi.dev/api/crew/")
+      .then((response) => response.json())
+      .then((data) => setCrew(data.data))
   }, []);
-
-  const showCharacters = characters.map((character) => {
-    if (character.lastname === "null") {
-      return (
-        <div>
-          <h3>{character.firstname}</h3>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <h3>
-            {character.firstname} {character.lastname}
-          </h3>
-        </div>
-      );
-    }
-  });
 
   return (
     <div className="App">
       <h1>Welcome to Office Trivia!</h1>
-      {showCharacters}
+      <Characters characters={characters} />
     </div>
   );
 }
