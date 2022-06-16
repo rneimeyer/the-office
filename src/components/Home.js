4b21740165cfafb72ddece480cfbe70381d9cb53
Rebecca Neimeyer <rebeccaneimeyer@Rebeccas-MacBook.local>
2022-06-16 12:42:03 -0700

    added basic site info to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-    return (
-        <div>Welcome to Office Trivia!</div>
-    )
+  return (
+    <div className="Home">
+      <h1>Welcome to Office Trivia!</h1>
+      <h2>Want to learn about different episodes and characters?</h2>
+      <h3>Visit our <Link to="/characters">Characters</Link> or <Link to="/episodes">Episodes</Link> pages.</h3>
+      <h2>Want to take quizzes on quotes and crew members?</h2>
+      <h3>Visit our <Link to="/quotes">Quotes</Link> or <Link to="/crew">Crew</Link> pages.</h3>
+    </div>
+  );
 }
 
 export default Home;
