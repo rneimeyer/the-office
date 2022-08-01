be7d29cf98b90d6eb13f3c21aeeb33b48b43fa7b
Rebecca Neimeyer <rebeccaneimeyer@Rebeccas-MacBook.local>
2022-08-01 15:22:29 -0700

    added new useState for choice, updated showQuote to load on first click, added new button to request a new question

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -3,35 +3,35 @@ import { useEffect, useState } from "react";
 function Quotes({ quotes, characters }) {
   const [show, setShow] = useState(false);
   const [randomQuote, setRandomQuote] = useState({});
+  const [choice, setChoice] = useState({});
 
   let firstOption = randomQuote.character;
 
   console.log(firstOption);
 
   const [secondOption, setSecondOption] = useState({});
-  const [correct, setCorrect] = useState("")
+  const [correct, setCorrect] = useState("");
 
   const getRandomQuote = () => {
     setRandomQuote(quotes[Math.floor(Math.random() * quotes.length)]);
+    setChoice(characters[Math.floor(Math.random() * characters.length)]);
   };
 
   const showQuote = () => {
+    setShow(true);
     getRandomQuote();
-    let choice = characters[Math.floor(Math.random() * characters.length)];
-    if (choice.firstname === firstOption.firstname) {
-      setSecondOption({ firstname: "this is a freebie!", lastname: "" });
+    // let choice = characters[Math.floor(Math.random() * characters.length)];
+    if (
+      choice.firstname === undefined ||
+      choice.firstname === firstOption.firstname
+    ) {
+      setSecondOption({ firstname: "This is a freebie!", lastname: "" });
     } else {
       setSecondOption(choice);
     }
-    setShow(true);
+    setCorrect("");
   };
-  const correctAnswer = () => {
 
-  }
-  const wrongAnswer = () => {
-
-  }
-console.log(correct)
   return (
     <div className="Quotes">
       <h1>Quotes</h1>
@@ -40,14 +40,38 @@ console.log(correct)
       {show ? (
         <div>
           <p>"{randomQuote.content}"</p>
-          <p onClick={()=>setCorrect("correct")}>
+          <p
+            onClick={() => {
+              setCorrect("true");
+            }}
+          >
             {firstOption.firstname} {firstOption.lastname}
           </p>
           <p>OR</p>
-          <p onClick={()=>setCorrect("wrong")}>
-            {secondOption.firstname} {secondOption.lastname}
+          <p
+            onClick={() => {
+              setCorrect("false");
+            }}
+          >
+            {secondOption.lastname == "null" ? (
+              <div>{secondOption.firstname}</div>
+            ) : (
+              <div>
+                {secondOption.firstname} {secondOption.lastname}
+              </div>
+            )}
           </p>
         </div>
+      ) : (
+        <div></div>
+      )}
+      {correct === "true" ? (
+        <div>
+          <p>correct!</p>
+          <button onClick={showQuote}>Next Quote</button>
+        </div>
+      ) : correct === "false" ? (
+        <div>try again! </div>
       ) : (
         <div></div>
       )}
